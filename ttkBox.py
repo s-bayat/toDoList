702ed3751066e94e0b2e6d2d69b3157867f34073
s-bayat <sajjad.bayat11@gmail.com>
2023-06-12 17:34:02 +0330

    edited

--- ttkBox.py
+++ ttkBox.py
@@ -4,7 +4,7 @@ import tkinter.ttk as ttk
 root = Tk()
 root.geometry("300x500")
 table = ttk.Treeview(root)
-table.grid(row=0, column=0, columnspan=3, pady=10, padx=10)
+table.grid(row=0, column=0, columnspan=3, pady=10, padx=30)
 table["columns"] = ("column 2", "column 3")
 table.column("#0", width=50, minwidth=20)
 table.column("column 2", width=90, minwidth=20)
