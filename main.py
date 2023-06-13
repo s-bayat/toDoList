c133d9931b52af3d81fe0e60e3ebf08ed11974d5
s-bayat <sajjad.bayat11@gmail.com>
2023-06-13 15:57:02 +0330

    edited

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from tkinter import ttk
+import tkinter.ttk as ttk
 
 window = Tk()
 window.title("To Do List")
@@ -12,4 +12,16 @@ task_entry = Entry(window)
 task_entry.grid(row=0, column=1)
 date_entry = Entry(window)
 date_entry.grid(row=1, column=1)
+
+table = ttk.Treeview(window)
+table.grid(row=2, column=0, columnspan=3, pady=10, padx=30)
+table["columns"] = ("column 2", "column 3")
+table.column("#0", width=50, minwidth=20)
+table.column("column 2", width=90, minwidth=20)
+table.column("column 3", width=90, minwidth=20)
+
+table.heading("#0", text="ID", anchor=W)
+table.heading("column 2", text="Task", anchor=W)
+table.heading("column 3", text="Date", anchor=W)
+
 window.mainloop()
--- ttkBox.py
+++ ttkBox.py
@@ -1,13 +1,17 @@
-from tkinter import *
-import tkinter.ttk as ttk
-
-root = Tk()
-root.geometry("300x500")
-table = ttk.Treeview(root)
-table.grid(row=0, column=0, columnspan=3, pady=10, padx=30)
-table["columns"] = ("column 2", "column 3")
-table.column("#0", width=50, minwidth=20)
-table.column("column 2", width=90, minwidth=20)
-table.column("column 3", width=90, minwidth=20)
-
-root.mainloop()
+# from tkinter import *
+# import tkinter.ttk as ttk
+#
+# root = Tk()
+# root.geometry("300x500")
+# table = ttk.Treeview(root)
+# table.grid(row=0, column=0, columnspan=3, pady=10, padx=30)
+# table["columns"] = ("column 2", "column 3")
+# table.column("#0", width=50, minwidth=20)
+# table.column("column 2", width=90, minwidth=20)
+# table.column("column 3", width=90, minwidth=20)
+#
+# table.heading("#0", text="ID", anchor=W)
+# table.heading("column 2", text="Task", anchor=W)
+# table.heading("column 3", text="Date", anchor=W)
+#
+# root.mainloop()
