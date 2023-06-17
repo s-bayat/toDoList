25a9d4b4499834feb1369e24ac2a477cc5ea1ab5
s-bayat <sajjad.bayat11@gmail.com>
2023-06-17 17:04:42 +0330

    edited

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ c = db_conn.cursor()
 # c.execute("""CREATE TABLE task_user(
 #             task text,
 #             date text
-#
-#
 # )""")
 db_conn.commit()
 
@@ -17,9 +15,9 @@ db_conn.commit()
 
 
 def add_task():
-    b_conn = sqlite3.connect("task.db")
-    C = db_conn.cursor()
-    C.execute("INSERT INTO task_user VALUES (:task_entry,:date_entry)",
+    db_conn = sqlite3.connect("task.db")
+    c = db_conn.cursor()
+    c.execute("INSERT INTO task_user VALUES (:task_entry,:date_entry)",
               {
                   "task_entry": task_entry.get(),
                   "date_entry": date_entry.get(),
@@ -31,30 +29,39 @@ def add_task():
     date_entry.delete(0, END)
 
 
+def query():
+    db_conn = sqlite3.connect("task.db")
+    c = db_conn.cursor()
+    table = ttk.Treeview(window)
+    table.grid(row=5, column=0, columnspan=2, pady=2, padx=10)
+    table["columns"] = ("column 2", "column 3")
+    table.column("#0", width=40, minwidth=20)
+    table.column("column 2", width=70, minwidth=20)
+    table.column("column 3", width=70, minwidth=20)
+
+    table.heading("#0", text="ID", anchor=W)
+    table.heading("column 2", text="Task", anchor=W)
+    table.heading("column 3", text="Date", anchor=W)
+    db_conn.commit()
+    db_conn.close()
+
+
 window = Tk()
 window.title("To Do List")
-window.geometry("300x500")
+window.geometry("250x550")
 task_label1 = Label(window, text="Enter Task :")
 task_label1.grid(row=0, column=0, padx=10, pady=10)
 task_label2 = Label(window, text="Enter Date :")
 task_label2.grid(row=1, column=0)
 task_entry = Entry(window)
-task_entry.grid(row=0, column=1)
+task_entry.grid(row=0, column=1, columnspan=2)
 date_entry = Entry(window)
 date_entry.grid(row=1, column=1)
 add_btn = Button(window, text="   Add Task   ", command=add_task)
 add_btn.grid(row=2, column=1, padx=5, pady=5)
+show_btn = Button(window, text=" Show Tasks ", command=query)
+show_btn.grid(row=3, column=1, padx=5, pady=5)
 del_btn = Button(window, text=" Delete Task ")
-del_btn.grid(row=3, column=1, padx=5, pady=5)
-table = ttk.Treeview(window)
-table.grid(row=4, column=0, columnspan=3, pady=10, padx=30)
-table["columns"] = ("column 2", "column 3")
-table.column("#0", width=50, minwidth=20)
-table.column("column 2", width=90, minwidth=20)
-table.column("column 3", width=90, minwidth=20)
-
-table.heading("#0", text="ID", anchor=W)
-table.heading("column 2", text="Task", anchor=W)
-table.heading("column 3", text="Date", anchor=W)
+del_btn.grid(row=4, column=1, padx=5, pady=5)
 
 window.mainloop()
